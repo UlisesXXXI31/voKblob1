8ec1a5ea51ab6841056c4e661c75b11f00a7d77e
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2026-01-22 12:41:05 +0100

    Update API base URL to include '/api' suffix

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-const API_BASE_URL = 'https://ls-api-b1.vercel.app';
+const API_BASE_URL = 'https://ls-api-b1.vercel.app/api';
 document.addEventListener("DOMContentLoaded", () => {
     // ---- LÓGICA DE AUTENTICACIÓN ----
     const token = localStorage.getItem('token');
